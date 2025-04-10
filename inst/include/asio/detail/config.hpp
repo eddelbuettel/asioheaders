a0f6aacce275451cfbd30d56f2fdc5e8619ed14f
Dirk Eddelbuettel <edd@debian.org>
2025-04-10 09:15:32 -0500

    Reapplied patch for macOS-13

--- inst/include/asio/detail/config.hpp
+++ inst/include/asio/detail/config.hpp
@@ -1411,11 +1411,7 @@
 // Standard library support for snprintf.
 #if !defined(ASIO_HAS_SNPRINTF)
 # if !defined(ASIO_DISABLE_SNPRINTF)
-#  if defined(__apple_build_version__)
-#    if (__clang_major__ >= 14)
-#     define ASIO_HAS_SNPRINTF 1
-#    endif // (__clang_major__ >= 14)
-#  endif // defined(__apple_build_version__)
+#   define ASIO_HAS_SNPRINTF 1
 # endif // !defined(ASIO_DISABLE_SNPRINTF)
 #endif // !defined(ASIO_HAS_SNPRINTF)
 
